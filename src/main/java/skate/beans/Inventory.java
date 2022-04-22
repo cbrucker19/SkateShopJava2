96b208c2eee9b1ec718d28f0f11400bbdd93691f
Suzette Senephansiri <93677348+Suzernator@users.noreply.github.com>
2022-04-22 18:51:49 -0500

    commit
    ch ch ch changes

--- src/main/java/skate/beans/Inventory.java
+++ src/main/java/skate/beans/Inventory.java
@@ -1,30 +1,17 @@
 package skate.beans;
-<<<<<<< HEAD
-import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-//Manuel Corral Ledezma
-=======
-
-import org.springframework.data.annotation.Id;
-
-import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
->>>>>>> 1653ea3 (commit)
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-<<<<<<< HEAD
-@Entity
-public class Inventory {
-	@Id
-=======
 @AllArgsConstructor
+
 public class Inventory {
 	@Id
 	@GeneratedValue
->>>>>>> 1653ea3 (commit)
 	private long itemId;
 }
