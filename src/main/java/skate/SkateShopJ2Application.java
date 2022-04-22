c441387424d79a4a82a51830bf3f0891dc49722d
chasevanblair <61804205+chasevanblair@users.noreply.github.com>
2022-04-21 21:32:42 -0500

    first

---
+++ src/main/java/skate/SkateShopJ2Application.java
@@ -0,0 +1,13 @@
+package skate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateShopJ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateShopJ2Application.class, args);
+	}
+
+}
---
+++ src/test/java/skate/SkateShopJ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package skate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkateShopJ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
